d3b77eba93366ee8c136c6990cb9b117b2d32080
jrh <joshuaharris1103@gmail.com>
2023-01-06 11:55:37 -0500

    renamed image file to rid of spaces, got the tiles to actually show images, working on timer and reset buttons.

--- js/app.js
+++ js/app.js
@@ -4,72 +4,72 @@ document.addEventListener('DOMContentLoaded', () => {
     // Photos and their randomization
     const cardList = [{
             name: 'NBA',
-            image: 'home/jrh/sei/projects/matching-pairs/Card-Matching-Game/NBA LOGOS FOLDER/nba-logo-transparent.png'
+            image: 'NBA_LOGOS_FOLDER/nba-logo-transparent.png'
         },
         {
             name: 'NBA',
-            image: 'home/jrh/sei/projects/matching-pairs/Card-Matching-Game/NBA LOGOS FOLDER/nba-logo-transparent.png'
+            image: 'NBA_LOGOS_FOLDER/nba-logo-transparent.png'
         },
 
         {
             name: 'easternConf',
-            image: 'home/jrh/sei/projects/matching-pairs/Card-Matching-Game/NBA LOGOS FOLDER/nba-Eastern_Conference_logo.png'
+            image: 'NBA_LOGOS_FOLDER/nba-Eastern_Conference_logo.png'
         },
         {
             name: 'easternConf',
-            image: 'home/jrh/sei/projects/matching-pairs/Card-Matching-Game/NBA LOGOS FOLDER/nba-Eastern_Conference_logo.png'
+            image: 'NBA_LOGOS_FOLDER/nba-Eastern_Conference_logo.png'
         },
 
         {
             name: 'Knicks',
-            image: 'home/jrh/sei/projects/matching-pairs/Card-Matching-Game/NBA LOGOS FOLDER/nba-new-york-knicks-logo.png'
+            image: 'NBA_LOGOS_FOLDER/nba-new-york-knicks-logo.png'
         },
         {
             name: 'Knicks',
-            image: 'home/jrh/sei/projects/matching-pairs/Card-Matching-Game/NBA LOGOS FOLDER/nba-new-york-knicks-logo.png'
+            image: 'NBA_LOGOS_FOLDER/nba-new-york-knicks-logo.png'
         },
         {
             name: 'Nets',
-            image: 'home/jrh/sei/projects/matching-pairs/Card-Matching-Game/NBA LOGOS FOLDER/nba-brooklyn-nets-logo.png'
+            image: 'NBA_LOGOS_FOLDER/nba-brooklyn-nets-logo.png'
         },
         {
             name: 'Nets',
-            image: 'home/jrh/sei/projects/matching-pairs/Card-Matching-Game/NBA LOGOS FOLDER/nba-brooklyn-nets-logo.png'
+            image: 'NBA_LOGOS_FOLDER/nba-brooklyn-nets-logo.png'
         },
 
         {
             name: '76ers',
-            image: 'home/jrh/sei/projects/matching-pairs/Card-Matching-Game/NBA LOGOS FOLDER/nba-philadelphia-76ers-logo.png'
+            image: 'NBA_LOGOS_FOLDER/nba-philadelphia-76ers-logo.png'
         },
         {
             name: '76ers',
-            image: 'home/jrh/sei/projects/matching-pairs/Card-Matching-Game/NBA LOGOS FOLDER/nba-philadelphia-76ers-logo.png'
+            image: 'NBA_LOGOS_FOLDER/nba-philadelphia-76ers-logo.png'
         },
 
         {
             name: 'Celtics',
-            image: 'home/jrh/sei/projects/matching-pairs/Card-Matching-Game/NBA LOGOS FOLDER/nba-boston-celtics-logo.png'
+            image: 'NBA_LOGOS_FOLDER/nba-boston-celtics-logo.png'
         },
         {
             name: 'Celtics',
-            image: 'home/jrh/sei/projects/matching-pairs/Card-Matching-Game/NBA LOGOS FOLDER/nba-boston-celtics-logo.png'
+            image: 'NBA_LOGOS_FOLDER/nba-boston-celtics-logo.png'
         },
 
         {
             name: 'Raptors',
-            image: 'home/jrh/sei/projects/matching-pairs/Card-Matching-Game/NBA LOGOS FOLDER/nba-toronto-raptors-logo-2020.png'
+            image: 'NBA_LOGOS_FOLDER/nba-toronto-raptors-logo-2020.png'
         },
         {
             name: 'Raptors',
-            image: 'home/jrh/sei/projects/matching-pairs/Card-Matching-Game/NBA LOGOS FOLDER/nba-toronto-raptors-logo-2020.png'
+            image: 'NBA_LOGOS_FOLDER/nba-toronto-raptors-logo-2020.png'
         },
         {
             name: 'Game Ball',
-            image: 'home/jrh/sei/projects/matching-pairs/Card-Matching-Game/NBA LOGOS FOLDER/nba-spalding-basketball.png'
+            image: 'NBA_LOGOS_FOLDER/nba-spalding-basketball.png'
         },
         {
             name: 'Game Ball',
-            image: 'home/jrh/sei/projects/matching-pairs/Card-Matching-Game/NBA LOGOS FOLDER/nba-spalding-basketball.png'
+            image: 'NBA_LOGOS_FOLDER/nba-spalding-basketball.png'
         }
     ]
     cardList.sort(() => 0.5 - Math.random())
@@ -79,6 +79,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const attemptsHolder = document.querySelector('.attempts')
     const foundHolder = document.querySelector('.finds')
     // const timeHolder = document.querySelector('.timer')
+    const gameButton = document.getElementById('#start')
     // const resetButton = document.querySelector('#reset')
     const gameCards = 8
 
@@ -92,31 +93,47 @@ document.addEventListener('DOMContentLoaded', () => {
 
     let selectedCards = []
     let selectedCardsIds = []
-
+    
+    // startGame = () => {
+    //     for (let i=0; i < cardList.length; i++)
+        
+    //     gameButton.addEventListener('click', initiateBoard)
+    //     // let time = setInterval(() => {
+            
+    //     // }, 1000)
+    //     // gametimer doesnt start until play is hit
+    //     // play button
+    
+    // initiateBoard()
+    // }
+    
     initiateBoard = () => {
         for (let i = 0; i < cardList.length; i++) {
             let card = document.createElement('img')
-            card.setAttribute('src', 'NBA LOGOS FOLDER/basket.png')
+            card.setAttribute('src', 'NBA_LOGOS_FOLDER/basket.png')
             card.setAttribute('data-id', i)
             card.addEventListener('click', flipCard)
             board.appendChild(card)
+            
         }
     }
+    
 
-
-    flipCard = () => {
-        if (selectedCards.length != 2) {
-            let cardId = this.getAttribute('data-id')
-            if (this.getAttribute('src') != 'NBA LOGOS FOLDER/blank.png') {
+    flipCard = (e) => {
+        if (selectedCards.length !== 2) {
+            let cardId = e.target.getAttribute('data-id')
+            console.log(e.target.getAttribute('src'))
+            if (e.target.getAttribute('src') !== 'NBA_LOGOS_FOLDER/blank.png') {
                 selectedCards.push(cardList[cardId].name)
                 selectedCardsIds.push(cardId)
-                this.SetAttribute('src', cardList[cardId].image)
+                e.target.setAttribute('src', cardList[cardId].image)
                 if (selectedCards.length == 2) {
                     setTimeout(checkMatches, 400)
                 }
             }
         }
     }
+    
 
     checkMatches = () => {
         attempts++
@@ -125,25 +142,32 @@ document.addEventListener('DOMContentLoaded', () => {
         let secondCard = selectedCardsIds[1]
         if (selectedCards[0] == selectedCards[1]) {
             foundCards++
-            cards[firstCard].setAttribute('src', 'NBA LOGOS FOLDER/blank.png')
-            cards[secondCard].setAttribute('src', 'NBA LOGOS FOLDER/blank.png')
+            cards[firstCard].setAttribute('src', 'NBA_LOGOS_FOLDER/blank.png')
+            cards[secondCard].setAttribute('src', 'NBA_LOGOS_FOLDER/blank.png')
         } else {
-            cards[firstCard].setAttribute('src', 'NBA LOGOS FOLDER/basket.png')
-            cards[secondCard].setAttribute('src', 'NBA LOGOS FOLDER/basket.png')
+            cards[firstCard].setAttribute('src', 'NBA_LOGOS_FOLDER/basket.png')
+            cards[secondCard].setAttribute('src', 'NBA_LOGOS_FOLDER/basket.png')
         }
         selectedCards = []
         selectedCardsIds = []
-        attemptsHolder.textContent = (`Attempts: ${attempts}`)
-        foundHolder.textContent = (`Finds: ${foundCards}`)
+        attemptsHolder.textContent = (`${attempts}`)
+        foundHolder.textContent = (`${foundCards}`)
+        // timeHolder.textContent = 
         if (foundCards == gameCards) {
             alert('completed')
         }
+    }
+
+    
+
+    gameOver = () => {
+
     }
 
     // resetBoard = () => {
 
     // }
 
-    initiateBoard()
+initiateBoard()
     // resetButton.addEventListener('click', resetBoard)
 })
